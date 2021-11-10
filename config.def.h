adbdf1bdde847e387f9e8c75618b7c15a2e5c3f7
oldwife <Spiky578@gmail.com>
2021-11-10 09:08:42 -0800

    just fixing git

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 
-/* -fn option overrides fonts[0]; default X11 font or font set */
+/* -fn 0ption overrides fonts[0]; default X11 font or font set */
 static char font[] = "monospace:size=10";
 static const char *fonts[] = {
 	font,
